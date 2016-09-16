2807c72482c656da63ba5b5081b113f0732f0ce6
Kevin Hawkins <khawkins@salesforce.com>
2016-09-16 15:05:38 -0700

    Initial framework creation

---
+++ TestCarthageReleaseFramework/TestCarthageReleaseFramework/TestCarthageReleaseFramework.h
@@ -0,0 +1,19 @@
+//
+//  TestCarthageReleaseFramework.h
+//  TestCarthageReleaseFramework
+//
+//  Created by Kevin Hawkins on 9/16/16.
+//  Copyright © 2016 Salesforce.com. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for TestCarthageReleaseFramework.
+FOUNDATION_EXPORT double TestCarthageReleaseFrameworkVersionNumber;
+
+//! Project version string for TestCarthageReleaseFramework.
+FOUNDATION_EXPORT const unsigned char TestCarthageReleaseFrameworkVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <TestCarthageReleaseFramework/PublicHeader.h>
+
+
---
+++ TestCarthageReleaseFramework/TestCarthageReleaseFrameworkTests/TestCarthageReleaseFrameworkTests.m
@@ -0,0 +1,39 @@
+//
+//  TestCarthageReleaseFrameworkTests.m
+//  TestCarthageReleaseFrameworkTests
+//
+//  Created by Kevin Hawkins on 9/16/16.
+//  Copyright © 2016 Salesforce.com. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface TestCarthageReleaseFrameworkTests : XCTestCase
+
+@end
+
+@implementation TestCarthageReleaseFrameworkTests
+
+- (void)setUp {
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    // Use XCTAssert and related functions to verify your tests produce the correct results.
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
